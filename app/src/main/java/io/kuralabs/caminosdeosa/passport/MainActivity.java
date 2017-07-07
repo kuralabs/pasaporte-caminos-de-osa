833fcab4cbba3439de0577de931ee331f20210e2
Carlos Jenkins <carlos.jenkins@kuralabs.io>
2017-07-07 01:08:13 -0600

    new: dev: Added base setup to support QR code reading using xzing.

--- app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
+++ app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
@@ -1,13 +1,44 @@
 package io.kuralabs.caminosdeosa.passport;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new IntentIntegrator(MainActivity.this).initiateScan();
+            }
+        });
     }
 }
