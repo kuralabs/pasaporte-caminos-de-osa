f8eb31b19ffb2f1f893088bdda10c21ddd6f020b
Carlos Jenkins <carlos.jenkins@kuralabs.io>
2017-07-08 10:49:05 -0600

    fix: dev: Delete unused files.

--- app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
+++ app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -62,7 +61,8 @@ public class MainActivity extends AppCompatActivity implements OnGestureListener
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_IMMERSIVE |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
 
         addStampButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
