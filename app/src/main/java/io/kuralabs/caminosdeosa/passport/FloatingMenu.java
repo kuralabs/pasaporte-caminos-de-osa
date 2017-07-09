021852853402cfcb20d4e2b3dc0f8a28c457ed67
Carolina Aguilar <carolina.aguilar@kuralabs.io>
2017-07-08 20:18:15 -0600

    new: dev: Show floating buttons according to current page.

---
+++ app/src/main/java/io/kuralabs/caminosdeosa/passport/FloatingMenu.java
@@ -0,0 +1,194 @@
+package io.kuralabs.caminosdeosa.passport;
+
+import android.animation.Animator;
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FloatingMenu implements IPickResult {
+
+    LinearLayout editFabLayout, shareFabLayout, addPhotoFabLayout, addStampFabLayout;
+    FloatingActionButton menuFab;
+    Map<String, String[]> floatingMenuConfig;
+    Map<String, LinearLayout> floatingLayouts;
+    Context context;
+    View fabOverlay;
+
+    //TODO: Pull the current page from the actual PageFlip component
+    String currentPage = "stamps_empty";
+
+    boolean isMenuOpen = false;
+
+    int floatingButtonsPosition[] = {
+        R.dimen.standard_55, R.dimen.standard_100, R.dimen.standard_145, R.dimen.standard_190
+    };
+
+    public FloatingMenu(Context mainContext, Map<String, LinearLayout> buttonsLayouts) {
+        floatingLayouts = buttonsLayouts;
+        context = mainContext;
+        setMenuConfig();
+    }
+
+
+    public void onShareClick(FloatingActionButton button) {
+        shareFabLayout = floatingLayouts.get("share");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    public void onEditClick(FloatingActionButton button) {
+        editFabLayout = floatingLayouts.get("edit");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    public void onAddStampClick(FloatingActionButton button, final Activity activity) {
+        addStampFabLayout = floatingLayouts.get("add");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    public void onAddPhotoClick(FloatingActionButton button, final Activity activity) {
+        addPhotoFabLayout = floatingLayouts.get("photo");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Pick Image
+                PickSetup setup = new PickSetup().setSystemDialog(true);
+                PickImageDialog.build(setup).show((FragmentActivity) activity);
+            }
+        });
+    }
+
+    public void onOpenMenuClick(FloatingActionButton button) {
+        menuFab = button;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isMenuOpen) {
+                    showFabMenu(floatingMenuConfig.get(currentPage));
+                } else {
+                    hideFabMenu();
+                }
+            }
+        });
+    }
+
+    public void onOverlayClick(View overlay) {
+        fabOverlay = overlay;
+        overlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideFabMenu();
+            }
+        });
+    }
+
+    private void setMenuConfig() {
+        floatingMenuConfig = new HashMap<String, String[]>();
+        floatingMenuConfig.put("cover", null);
+        floatingMenuConfig.put("data", new String[]{ "edit" });
+        floatingMenuConfig.put("manifesto", null);
+        floatingMenuConfig.put("stamps", new String[]{ "share", "photo" });
+        floatingMenuConfig.put("stamps_empty", new String[]{ "add" });
+    }
+
+    private void showFabMenu(String[] actions) {
+        isMenuOpen = true;
+
+        if (actions != null) {
+
+            fabOverlay.setVisibility(View.VISIBLE);
+            menuFab.animate().rotationBy(180);
+
+            for (int i = 0; i < actions.length; i++) {
+                LinearLayout button = floatingLayouts.get(actions[i]);
+                button.setVisibility(View.VISIBLE);
+                button.animate().translationY(-context.getResources().getDimension(floatingButtonsPosition[i]));
+            }
+        }
+    }
+
+    private void hideFabMenu() {
+        isMenuOpen = false;
+
+        fabOverlay.setVisibility(View.GONE);
+        menuFab.animate().rotationBy(-180);
+        shareFabLayout.animate().translationY(0);
+        addPhotoFabLayout.animate().translationY(0);
+        addStampFabLayout.animate().translationY(0);
+        editFabLayout.animate().translationY(0).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                if (!isMenuOpen) {
+                    for (LinearLayout l : floatingLayouts.values()) {
+                        l.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onPickResult(PickResult r) {
+        if (r.getError() == null) {
+            //If you want the Uri.
+            //Mandatory to refresh image from Uri.
+            //getImageView().setImageURI(null);
+
+            //Setting the real returned image.
+            //getImageView().setImageURI(r.getUri());
+
+            //If you want the Bitmap.
+
+            // getImageView().setImageBitmap(r.getBitmap());
+            Toast.makeText(context, r.getPath(), Toast.LENGTH_LONG).show();
+            //Image path
+            //r.getPath();
+        } else {
+            //Handle possible errors
+            //TODO: do what you have to do with r.getError();
+            Toast.makeText(context, r.getError().getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
+++ app/src/main/java/io/kuralabs/caminosdeosa/passport/MainActivity.java
@@ -1,6 +1,5 @@
 package io.kuralabs.caminosdeosa.passport;
 
-import android.animation.Animator;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -16,34 +15,16 @@ import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.integration.android.IntentIntegrator;
 
 import com.vansuita.pickimage.bean.PickResult;
-import com.vansuita.pickimage.dialog.PickImageDialog;
-import com.vansuita.pickimage.listeners.IPickResult;
-import com.vansuita.pickimage.bundle.PickSetup;
 
 import io.kuralabs.caminosdeosa.passport.flip.BookView;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements IPickResult {
 
-    FloatingActionButton menuFab, editFab, shareFab, addPhotoFab, addStampFab;
-    LinearLayout fabLayouts[], editFabLayout, shareFabLayout, addPhotoFabLayout, addStampFabLayout;
+public class MainActivity extends AppCompatActivity {
+
     BookView bookView;
     View decorView;
-
-    View fabOverlay;
-    boolean isMenuOpen = false;
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            String message = result.getContents() == null ?
-                    "Cancelled" :
-                    "Scanned: " + result.getContents();
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,124 +42,9 @@ public class MainActivity extends AppCompatActivity implements IPickResult {
 
         setContentView(passportView);
 
-        setupMenuFab();
-
+        initMenu();
     }
 
-    private void setupMenuFab() {
-        shareFabLayout = (LinearLayout) findViewById(R.id.shareFabLayout);
-        addPhotoFabLayout = (LinearLayout) findViewById(R.id.addPhotoFabLayout);
-        addStampFabLayout = (LinearLayout) findViewById(R.id.addStampFabLayout);
-        editFabLayout = (LinearLayout) findViewById(R.id.editFabLayout);
-
-        shareFab = (FloatingActionButton) findViewById(R.id.shareFab);
-        shareFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        addPhotoFab= (FloatingActionButton) findViewById(R.id.addPhotoFab);
-        addPhotoFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Pick Image
-                PickSetup setup = new PickSetup().setSystemDialog(true);
-                PickImageDialog.build(setup).show(MainActivity.this);
-            }
-        });
-
-        addStampFab = (FloatingActionButton) findViewById(R.id.addStampFab);
-        addStampFab.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Scan QR
-                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.initiateScan();
-            }
-        });
-
-        editFab = (FloatingActionButton) findViewById(R.id.editFab);
-        editFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        menuFab = (FloatingActionButton) findViewById(R.id.menuFab);
-        menuFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!isMenuOpen){
-                    showFabMenu();
-                } else {
-                    hideFabMenu();
-                }
-            }
-        });
-
-        fabOverlay = findViewById(R.id.fabBGLayout);
-        fabOverlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideFabMenu();
-            }
-        });
-
-        fabLayouts = new LinearLayout[] {
-            shareFabLayout, addStampFabLayout, addPhotoFabLayout, editFabLayout
-        };
-    }
-
-    private void showFabMenu() {
-        isMenuOpen = true;
-
-        for (LinearLayout l : fabLayouts) {
-            l.setVisibility(View.VISIBLE);
-        }
-        fabOverlay.setVisibility(View.VISIBLE);
-
-        menuFab.animate().rotationBy(180);
-        shareFabLayout.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
-        addPhotoFabLayout.animate().translationY(-getResources().getDimension(R.dimen.standard_100));
-        addStampFabLayout.animate().translationY(-getResources().getDimension(R.dimen.standard_145));
-        editFabLayout.animate().translationY(-getResources().getDimension(R.dimen.standard_190));
-    }
-
-    private void hideFabMenu() {
-        isMenuOpen = false;
-
-        fabOverlay.setVisibility(View.GONE);
-        menuFab.animate().rotationBy(-180);
-        shareFabLayout.animate().translationY(0);
-        addPhotoFabLayout.animate().translationY(0);
-        addStampFabLayout.animate().translationY(0);
-        editFabLayout.animate().translationY(0).setListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                if (!isMenuOpen) {
-                    for (LinearLayout l : fabLayouts) {
-                        l.setVisibility(View.GONE);
-                    }
-                }
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -206,25 +72,33 @@ public class MainActivity extends AppCompatActivity implements IPickResult {
     }
 
     @Override
-    public void onPickResult(PickResult r) {
-        if (r.getError() == null) {
-            //If you want the Uri.
-            //Mandatory to refresh image from Uri.
-            //getImageView().setImageURI(null);
-
-            //Setting the real returned image.
-            //getImageView().setImageURI(r.getUri());
-
-            //If you want the Bitmap.
-
-            // getImageView().setImageBitmap(r.getBitmap());
-            Toast.makeText(this, r.getPath(), Toast.LENGTH_LONG).show();
-            //Image path
-            //r.getPath();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String message = result.getContents() == null ?
+                    "Cancelled" :
+                    "Scanned: " + result.getContents();
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         } else {
-            //Handle possible errors
-            //TODO: do what you have to do with r.getError();
-            Toast.makeText(this, r.getError().getMessage(), Toast.LENGTH_LONG).show();
+            super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+
+    // Menu Setup
+    private void initMenu() {
+        Map<String, LinearLayout> floatingButtons = new HashMap<String, LinearLayout>();
+        floatingButtons.put("share", (LinearLayout) findViewById(R.id.shareFabLayout));
+        floatingButtons.put("add", (LinearLayout) findViewById(R.id.addStampFabLayout));
+        floatingButtons.put("photo", (LinearLayout) findViewById(R.id.addPhotoFabLayout));
+        floatingButtons.put("edit", (LinearLayout) findViewById(R.id.editFabLayout));
+
+        FloatingMenu menu = new FloatingMenu(this, floatingButtons);
+        menu.onShareClick((FloatingActionButton) findViewById(R.id.shareFab));
+        menu.onEditClick((FloatingActionButton) findViewById(R.id.editFab));
+        menu.onAddPhotoClick((FloatingActionButton) findViewById(R.id.addPhotoFab), this);
+        menu.onAddStampClick((FloatingActionButton) findViewById(R.id.addStampFab), this);
+        menu.onOpenMenuClick((FloatingActionButton) findViewById(R.id.menuFab));
+        menu.onOverlayClick(findViewById(R.id.fabBGLayout));
     }
 }
